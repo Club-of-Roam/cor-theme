9ca441365ff208ad9be54ae107dcf07ab78b4271
Pille <j.pilkahn@vivaconagua.org>
2015-05-22 16:48:06 +0200

    Temporarily disabled update nags - parent theme is incompatible with WP 4.0 and up. Pille is suspecting jQuery(UI) to be the culprit...

--- functions.php
+++ functions.php
@@ -233,4 +233,20 @@ remove_action('show_user_profile', array($sitepress, 'show_user_options'));
 
 // none so far
 
+
+/* UPDATE NAG
+----------------------------------------- */
+
+// temporary "fix"
+// The current (parent) theme is incompatible with WordPress 4.0 and up...
+
+function remove_core_updates()
+{
+	global $wp_version;
+	return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
+
 ?>
