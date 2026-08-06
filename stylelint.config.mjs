bc4758c25bec4c3738f234235c94e65c8037a224
mafiacoder <m.boschert@arcor.de>
2026-08-06 22:47:39 +0200

    use @wordpress/stylelint-config instead of @wordpress/stylelint-config/stylistic because of prettier conflicts

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('stylelint').Config} */
 export default {
-	extends: ['@wordpress/stylelint-config/stylistic'],
+	extends: ['@wordpress/stylelint-config'],
 	rules: {
 		'selector-class-pattern': ['^([a-z][a-z0-9]*)([-_][a-z0-9]+)*$'],
 		'no-descending-specificity': null,
