71cd4c98c6399d09b0060e1761b5e4b21250be9b
mafiacoder <m.boschert@arcor.de>
2026-08-06 16:43:01 +0200

    fixed baselineAdjust

--- js/pille-baseline-grid.js
+++ js/pille-baseline-grid.js
@@ -2,28 +2,28 @@
 	const baselineAdjust = () => {
 		const viewportWidth = window.outerWidth,
 			verticalRhythm = 23;
-		$('.content-container img').each(() => {
-			const imgHeight = $(this).height();
+		$('.content-container img').each((_, element) => {
+			const imgHeight = $(element).height();
 			if (
-				$(this).parent().is('p') &&
-				!$(this).hasClass('no-bsl-adjust')
+				$(element).parent().is('p') &&
+				!$(element).hasClass('no-bsl-adjust')
 			) {
 				const pImgBaselineOffset =
 					(imgHeight - (verticalRhythm - verticalRhythm / 3)) %
 					verticalRhythm;
 				if (pImgBaselineOffset !== 0) {
-					const imgWidth = $(this).width();
+					const imgWidth = $(element).width();
 					const resizeRatio =
 						(imgHeight - pImgBaselineOffset) / imgHeight;
-					$(this).css('height', imgHeight - pImgBaselineOffset);
-					$(this).css('width', Math.round(imgWidth * resizeRatio));
+					$(element).css('height', imgHeight - pImgBaselineOffset);
+					$(element).css('width', Math.round(imgWidth * resizeRatio));
 				}
 			} else if (
 				viewportWidth > 767 &&
-				$(this).hasClass('baseline-adjustable')
+				$(element).hasClass('baseline-adjustable')
 			) {
 				const imgBaselineOffset = imgHeight % verticalRhythm;
-				$(this).css(
+				$(element).css(
 					'margin-bottom',
 					2 * verticalRhythm - imgBaselineOffset + 'px'
 				);
