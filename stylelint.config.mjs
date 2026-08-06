f17427b5fb1bb5d3edbf732b4cb006bf662bf3a8
mafiacoder <m.boschert@arcor.de>
2026-08-06 19:55:53 +0200

    added stylelint to lint css

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: ['@wordpress/stylelint-config/stylistic'],
+	rules: {
+		'selector-class-pattern': ['^([a-z][a-z0-9]*)([-_][a-z0-9]+)*$'],
+		'no-descending-specificity': null,
+	},
+};
